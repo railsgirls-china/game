672a5c4f2d6663a32a76c9a54f68031a1909472a
qinmingyuan <mingyuan0715@foxmail.com>
2015-08-22 10:01:25 +0800

    add role for users

--- app/models/user_gift.rb
+++ app/models/user_gift.rb
@@ -2,14 +2,4 @@ class UserGift < ActiveRecord::Base
   belongs_to :user
   belongs_to :gift
 
-  validate :users_must_less_than_amount, on: :create
-
-  def users_must_less_than_amount
-
-    if self.class.where(gift_id: self.gift_id).count > self.gift.amount
-      errors.add "人数超过了"
-    end
-
-  end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821150615) do
+ActiveRecord::Schema.define(version: 20150822015625) do
 
   create_table "gifts", force: :cascade do |t|
     t.string   "grade",      limit: 255
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150821150615) do
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "role",       limit: 255
   end
 
 end
